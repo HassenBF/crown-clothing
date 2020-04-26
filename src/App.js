f2d4dcc554713a18d8b64f7c55010659939a7793
benf.hassen@gmail.com <11031989Hb10!:ghub>
2020-04-26 13:27:40 +0200

    Added header component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
+import {Route,Switch} from 'react-router-dom';
 import HomePage from "./pages/homepage/homepage.component";
-import {Route,Switch} from 'react-router-dom'
 import ShopPage from "./pages/shop/shop.component";
+import Header  from "./components/header/header.component";
+
 
 function App() {
   return (
     <div >
+        {/*by putting header outside of switch we ensure that it's always rendered*/}
+        <Header/>
         <Switch>
             <Route exact path='/' component={HomePage}/>
             <Route path='/shop' component={ShopPage}/>
